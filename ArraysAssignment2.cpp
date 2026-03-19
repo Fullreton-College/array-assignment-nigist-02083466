7048aeb7ce6d694f4977742f07ff225398a8225c
Tekle, Nigist <nigist.tekle21@gmail.com>
2026-03-18 20:15:57 -0700

    code changes complete

--- ArraysAssignment2.cpp
+++ ArraysAssignment2.cpp
@@ -13,6 +13,20 @@ int main() {
     cout << "Enter up to 20 scores (0-100). Enter -1 to stop:\n";
 
     // Store scores and keep track of how many are entered
+    while (count < 20)
+    {
+        cin >> input;
+
+        if (input == -1)
+            break;
+
+        if (input >= 0 && input <= 100)
+        {
+            scores[count] = input;
+            count++;
+        }
+    }
+
     // Report perfect scores using the helper function
   // COMPLETE THE FUNCTION BELOW
   int perfectOnes = countPerfect(scores, count);
@@ -27,5 +41,12 @@ int main() {
 int countPerfect(const int scores[], int size) {
     int totalPerfect = 0;
     //COMPLETE THE CODE 
+    for (int i = 0; i < size; i++)
+    {
+        if (scores[i] == 100)
+        {
+            totalPerfect++;
+        }
+    }
     return totalPerfect;
 }

2e69ab60dede3b9e100f8cb6ac04b08d76348e1f
Tekle, Nigist <nigist.tekle21@gmail.com>
2026-03-18 20:05:50 -0700

    code changes complete

--- ArraysAssignment1.cpp
+++ ArraysAssignment1.cpp
@@ -1,5 +1,10 @@
 //add includes
+#include <iostream>
+#include <string>
+
 //add namespace declarations
+using namespace std;
+
 string lookupName(string targetName, string names[], string phoneNumbers[], int size);
 
 int main()
@@ -29,10 +34,21 @@ int main()
                       << endl;
          cout << "Look up another name? (y/n)"
               << endl;
-      cin > c;
+      cin >> c;
       cin.ignore();
  } while (c == 'y');
  return 0;
 }
 
-//complete lookupName unction
+//complete lookupName function
+string lookupName(string targetName, string names[], string phoneNumbers[], int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        if (names[i] == targetName)
+        {
+            return phoneNumbers[i];
+        }
+    }
+    return "";
+}
